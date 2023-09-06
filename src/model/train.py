5140478ee03114414d82b53209b558f9c2ca5853
Himanshu <himanshu16397@gmail.com>
2023-09-06 18:00:31 +0000

    fix linting

--- src/model/train.py
+++ src/model/train.py
@@ -1,16 +1,18 @@
 # Import libraries
 
+
 import argparse
 import glob
 import os
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy
 import mlflow
 import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
-
-
 # define functions
+
+
 def main(args):
     # TO DO: enable autologging
     mlflow.sklearn.autolog()
@@ -35,10 +37,12 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-def split_data(df:'Dataframe')->'train and test arrays':
-    X,y=df.drop('Diabetic',axis=1).values,df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=0)
+def split_data(df: pd.core.frame.DataFrame) -> numpy.ndarray:
+    X, y = df.drop('Diabetic', axis=1).values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,
+                                                        random_state=0)
     return X_train, X_test, y_train, y_test
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -60,6 +64,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
