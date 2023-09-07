4fdf1862d17aeb82824de34d193cd0c8639edee2
Himanshu <himanshu16397@gmail.com>
2023-09-07 21:01:08 +0530

    fix job status logic

--- check_job_status.py
+++ check_job_status.py
@@ -1,6 +1,37 @@
 # Run: python check_job_status.py
+
+# import the required libraries
 import json
-# read the json file
+import os
+import argparse
+import time
+
+
+#new logic
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name",dest='name', type=str)
+args = parser.parse_args()
+job_id = args.name
+print('job id is', job_id)
+
+while True:    
+    value=os.popen(f'az ml job show -n {job_id} \
+                    -w learnML -g myResourcegroupforML').read()
+    status=json.loads(value)['status']
+    if status == 'Running':
+        print('Job is running')
+        time.sleep(60)
+    elif status == 'Failed':
+        print('Job is failed')
+        break
+    elif status == 'Completed':
+        print('Job is completed')
+        break
+
+# old logic
+"""# read the json file
 dict = json.loads(open('job_status.json').read())
 # print the status
 
@@ -18,4 +49,4 @@ while dict['status'] == 'Running':
         break
     elif dict['status'] == 'Completed':
         print('Job is completed')
-        break
+        break"""
