7fcf5694f06097fa4821dbc7ac202ac7b018ab9c
Himanshu <himanshu16397@gmail.com>
2023-11-02 14:49:14 +0530

    lint train file

--- src/model/train.py
+++ src/model/train.py
@@ -46,7 +46,8 @@ def split_data(df: pd.core.frame.DataFrame) -> numpy.ndarray:
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train
+                                                               )
 
 
 def parse_args():
